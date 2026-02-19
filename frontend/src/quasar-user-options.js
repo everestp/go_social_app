8768db8b82e534a1e99de89fd1ae2183b5f6625a
everestp <paudeleverest09@gmail.com>
2026-02-19 11:24:55 +0545

    Intial commit

---
+++ frontend/src/quasar-user-options.js
@@ -0,0 +1,12 @@
+
+import './styles/quasar.sass'
+import iconSet from 'quasar/icon-set/eva-icons.js'
+import '@quasar/extras/eva-icons/eva-icons.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
